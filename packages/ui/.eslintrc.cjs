ae86afbce9b44dfd45be4f4e07bb6e7581d063b7
dohun31 <dohun31@naver.com>
2025-07-24 22:28:26 +0900

    fix: eslint 잘못된 경로 수정

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   ignorePatterns: [".eslintrc.cjs"],
-  extends: ["@repo/eslint-config/index.js"],
+  extends: ["@gotchai/eslint-config"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
