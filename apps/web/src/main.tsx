d7ea9ca8cd3be48f6ec408f840514ab39d830430
dohun31 <dohun31@naver.com>
2025-07-24 22:22:41 +0900

    feat: web css 파일에 ui 패키지 빌드된 css 결과물 import

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,22 +1,10 @@
+import { Counter } from "@gotchai/ui";
 import { createRoot } from "react-dom/client";
+
 import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Header } from "@gotchai/ui/components/header";
-import { Counter } from "@gotchai/ui/components/counter";
 
 const App = () => (
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-    </a>
-    <Header title="Web" />
     <div className="card">
       <Counter />
     </div>
