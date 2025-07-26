bf4a125a9c635f19c5adfb1ade0c07fcb34f9d20
홍규진 <hkj0206@dgu.ac.kr>
2025-07-26 16:22:13 +0900

    chore: 잡다한 수정

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,6 @@
 import "./styles";
 import "@gotchai/ui/style.css";
-import TypographyExample from "./typography-example";
+import { TypographyExample } from "./typography-example";
 
 export const App = () => {
   return (
