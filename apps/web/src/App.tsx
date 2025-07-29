659840523abaeafbd95e5ede73c434f603eb915e
홍규진 <hkj0206@dgu.ac.kr>
2025-07-29 18:29:35 +0900

    refactor: comment 반영 - modal 컴포넌트 wrapping

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,32 +1,31 @@
 import "@gotchai/ui/style.css";
 import { TypographyExample } from "./typography-example";
-import { Button, Chip, Modal } from "@gotchai/ui";
+import { Button, Chip } from "@gotchai/ui";
 import quizCorrect from "./assets/quiz-correct.png";
 import quizWrong from "./assets/quiz-wrong.png";
 import "./styles";
 import { useState } from "react";
-import { DefaultModal } from "./components/DefaultModal/DefaultModal";
+import { QuizResultModal } from "./components/QuizResultModal/QuizResultModal";
 
 export const App = () => {
-  const [isModalOpenCorrect, setIsModalOpenCorrect] = useState(false);
-  const [isModalOpenWrong, setIsModalOpenWrong] = useState(false);
-
+  const [isCorrectModalOpen, setIsCorrectModalOpen] = useState(false);
+  const [isWrongModalOpen, setIsWrongModalOpen] = useState(false);
   const handleOpenModal = () => {
-    setIsModalOpenCorrect(true);
+    setIsCorrectModalOpen(true);
   };
 
   const handleOpenModalWrong = () => {
-    setIsModalOpenWrong(true);
+    setIsWrongModalOpen(true);
   };
 
   const handleCloseModal = () => {
-    setIsModalOpenCorrect(false);
-    setIsModalOpenWrong(false);
+    setIsCorrectModalOpen(false);
+    setIsWrongModalOpen(false);
   };
 
   const handleClickNext = () => {
-    setIsModalOpenCorrect(false);
-    setIsModalOpenWrong(false);
+    setIsCorrectModalOpen(false);
+    setIsWrongModalOpen(false);
   };
 
   return (
@@ -35,24 +34,22 @@ export const App = () => {
       <Chip color="blue">1/7</Chip>
       <Button onClick={handleOpenModal}>정답 버튼</Button>
       <Button onClick={handleOpenModalWrong}>오답 버튼</Button>
-      <Modal isOpen={isModalOpenCorrect} onClose={handleCloseModal}>
-        <DefaultModal
-          isOpen={isModalOpenCorrect}
-          title="정답공개"
-          answer="어떤 빵을 사셨나요? 다음엔 제가 추천드릴수도 있어요"
-          icon={<img src={quizCorrect} alt="check" />}
-          onClickNext={handleClickNext}
-        />
-      </Modal>
-      <Modal isOpen={isModalOpenWrong} onClose={handleCloseModal}>
-        <DefaultModal
-          isOpen={isModalOpenWrong}
-          title="오답"
-          answer="음~ 반짝이랑 리본 살짝 감으면 확 살아날 것 같은데?"
-          icon={<img src={quizWrong} alt="check" />}
-          onClickNext={handleClickNext}
-        />
-      </Modal>
+      <QuizResultModal
+        isOpen={isCorrectModalOpen}
+        title="정답공개"
+        answer="어떤 빵을 사셨나요? 다음엔 제가 추천드릴수도 있어요"
+        icon={<img src={quizCorrect} alt="check" />}
+        onClickNext={handleClickNext}
+        onClose={handleCloseModal}
+      />
+      <QuizResultModal
+        isOpen={isWrongModalOpen}
+        title="오답"
+        answer="음~ 반짝이랑 리본 살짝 감으면 확 살아날 것 같은데?"
+        icon={<img src={quizWrong} alt="check" />}
+        onClickNext={handleClickNext}
+        onClose={handleCloseModal}
+      />
     </div>
   );
 };
