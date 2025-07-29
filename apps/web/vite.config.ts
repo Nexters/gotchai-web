4e02e8c568599d943a4e19ce9945c0e3693eab99
홍규진 <hkj0206@dgu.ac.kr>
2025-07-30 00:13:07 +0900

    feat: icon 사용을 위한 svgr() 세팅

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), react()],
+  plugins: [
+    vanillaExtractPlugin(),
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
 });
