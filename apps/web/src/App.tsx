b072619f7fb7f1af13222f450cde82f90c0f94fc
홍규진 <hkj0206@dgu.ac.kr>
2025-07-29 23:31:16 +0900

    feat: components 페이지

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,11 +4,13 @@ import "@gotchai/ui/style.css";
 
 import "./styles";
 import { IntroPage } from "./pages/Intro";
+import { ComponentsPage } from "./pages/Components";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<IntroPage />} />
+      <Route path="/components" element={<ComponentsPage />} />
     </Routes>
   );
 };
---
+++ apps/web/src/pages/Components/index.tsx
@@ -0,0 +1,11 @@
+import { ResultCard } from "../../components/ResultCard/ResultCard";
+
+export const ComponentsPage = () => {
+  return (
+    <div>
+      <ResultCard result="gold" />
+      <ResultCard result="silver" />
+      <ResultCard result="bronze" />
+    </div>
+  );
+};
