5a91fe545c25ddc567524fceafb7b448f1f57632
hodun <65100540+dohun31@users.noreply.github.com>
2025-08-16 22:22:35 +0900

    feat: 로딩 로띠 적용 (#47)

--- apps/web/src/components/Loading/Loading.tsx
+++ apps/web/src/components/Loading/Loading.tsx
@@ -1,24 +1,35 @@
 import { Text } from "@gotchai/ui";
+import Lottie from "lottie-react";
+
 import {
-  loadingContainer,
-  loadingPlaceholder,
-  textContainer,
-  textBackground,
-  textOutline,
+	loadingContainer,
+	textContainer,
+	textBackground,
+	textOutline,
 } from "./style.css";
+import lottie from "./lottie.json";
 
 export const Loading = () => {
-  return (
-    <div className={loadingContainer}>
-      <div className={loadingPlaceholder}></div>
-      <div className={textContainer}>
-        <Text size="xl" weight="medium" className={textBackground}>
-          당신의 Ai력 판단중...
-        </Text>
-        <Text size="xl" weight="medium" className={textOutline}>
-          당신의 Ai력 판단중...
-        </Text>
-      </div>
-    </div>
-  );
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: lottie,
+		rendererSettings: {
+			preserveAspectRatio: "xMidYMid slice",
+		},
+	};
+
+	return (
+		<div className={loadingContainer}>
+			<Lottie {...defaultOptions} />
+			<div className={textContainer}>
+				<Text size="xl" weight="medium" className={textBackground}>
+					당신의 Ai력 판단중...
+				</Text>
+				<Text size="xl" weight="medium" className={textOutline}>
+					당신의 Ai력 판단중...
+				</Text>
+			</div>
+		</div>
+	);
 };
