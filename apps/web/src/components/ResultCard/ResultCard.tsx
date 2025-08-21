8ec420feabde99d5bb77beb5500fa24328729d17
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-08-21 17:42:56 +0900

    Refactor/#40 - 결과 이미지 수정 및 클립보드 복사시 배경 추가 (#46)
    * chore: image 교체
    
    * feat: ResultCard 클립보드 이미지 추출시 배경 색 추가
    
    * feat: ResultCard 클립보드 이미지 추출시 배경 색 추가 - 2 ref 위치 변경
    
    * feat: ResultCard 너비 및 높이 상수화
    
    * fix: 정책 변경으로 인해 퀴즈 정답 메시지 변환

--- apps/web/src/components/ResultCard/ResultCard.tsx
+++ apps/web/src/components/ResultCard/ResultCard.tsx
@@ -4,40 +4,65 @@ import {
   cardContainer,
   cardContent,
   cardImage,
+  cardContentContainer,
   cardLinearBackground,
-  cardLogo,
+  cardLogoContainer,
   cardRadialGradient,
-  cardText
+  cardText,
+  cardTextContent,
 } from "./style.css";
 import { CardResult, CARD_CONTENTS } from "./cardConfig";
 import { CONTENT_COLORS } from "./cardConfig";
 import Logo from "../Logo";
+import { forwardRef } from "react";
+import { QUIZZES } from "../../pages/test/constants";
 
 interface CardProps {
   result: CardResult;
+  correctCount: number;
 }
 
-export const ResultCard = ({ result }: CardProps) => {
-  const imageSrc = `/assets/${result}.png`;
+export const ResultCard = forwardRef<HTMLDivElement, CardProps>(
+  ({ result, correctCount }, ref) => {
+    // 동적 이미지 소스 설정
+    const imageSrc = `/assets/${result}.png`;
+    // 동적 subTitle 생성
+    const dynamicSubTitle =
+      `${QUIZZES.length}개중 ${correctCount}개를 맞춘 당신은` ||
+      CARD_CONTENTS[result].subTitle;
 
-  return (
-    <div className={cardContainer}>
-      <div className={cardLinearBackground} />
-      <div className={cardRadialGradient({ result })} />
-      <div className={cardContent}>
-        <img src={imageSrc} className={cardImage} alt="card" />
-        <Text size="xl" weight="medium" color={CONTENT_COLORS[result].subTitle}>
-          {CARD_CONTENTS[result].subTitle}
-        </Text>
-        
-        <Title order={3} color={CONTENT_COLORS[result].title}>
-          {CARD_CONTENTS[result].title}
-        </Title>
-        <Text size="lg" weight="medium" className={cardText}>
-          {CARD_CONTENTS[result].description}
-        </Text>
-        <Logo fill={CONTENT_COLORS[result].title} className={cardLogo} />
+    return (
+      <div className={cardContainer} ref={ref}>
+        <div className={cardLinearBackground} />
+        <div className={cardRadialGradient({ result })} />
+        <div className={cardContent}>
+          <div className={cardContentContainer}>
+            <img src={imageSrc} className={cardImage} alt="card" />
+            <div className={cardTextContent}>
+              <Text
+                size="xl"
+                weight="medium"
+                color={CONTENT_COLORS[result].subTitle}
+              >
+                {dynamicSubTitle}
+              </Text>
+              <Title order={3} color={CONTENT_COLORS[result].title}>
+                {CARD_CONTENTS[result].title}
+              </Title>
+              <Text size="lg" weight="medium" className={cardText}>
+                {CARD_CONTENTS[result].description}
+              </Text>
+            </div>
+          </div>
+          <div className={cardLogoContainer}>
+            <Logo
+              width={56}
+              height={12}
+              fill={CONTENT_COLORS[result].subTitle}
+            />
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
