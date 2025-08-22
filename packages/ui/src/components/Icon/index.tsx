7e12aa7561215a9d4f13cfe0f095771dea7cdf94
홍규진 <hkj0206@dgu.ac.kr>
2025-08-22 22:42:10 +0900

    hotfix: Icon 수정

--- packages/ui/src/components/Icon/index.tsx
+++ packages/ui/src/components/Icon/index.tsx
@@ -1,9 +1,11 @@
 import { CheckIcon } from "./CheckIcon";
 import { ResetIcon } from "./ResetIcon";
 import { SaveIcon } from "./SaveIcon";
+import { ShareIcon } from "./ShareIcon";
 
 export const Icon = Object.assign(() => <></>, {
 	Check: CheckIcon,
 	Reset: ResetIcon,
 	Save: SaveIcon,
+	Share: ShareIcon,
 });
